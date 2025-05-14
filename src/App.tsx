da440d09c1621711df9d8736604510da26a3f138
Chiril Luncasu <chiril.luncasu@gmail.com>
2025-05-14 13:32:09 +0200

    Initial commit: Vite + React + PWA scaffold

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>🛒 ReStockr</h1>
+      <p>This is the bare-bones grocery tracker app.</p>
+    </div>
+  );
+}
+
+export default App;
